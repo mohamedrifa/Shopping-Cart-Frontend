6311a98e97c118d5673e1e0a0507bf0448da2f9c
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-11-08 10:54:03 +0530

    final changejah

--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -1,6 +1,8 @@
+import { Navigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
 export default function PrivateRoute({ children }) {
   const token = Cookies.get("token");
-  return token ? children : <Navigate to="/login" />;
+
+  return token ? children : <Navigate to="/login" replace />;
 }
